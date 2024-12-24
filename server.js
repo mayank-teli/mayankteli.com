c377d8448e3d689e40909101c224c3253e093272
Mayank Teli <mayankmodi430@gmail.com>
2024-12-25 00:38:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-function SendMail(){
-    var parms ={
-        name : document.getElementById("name").Value,
-        email : document.getElementById("email").Value,
-        message : document.getElementById("message").Value
+function sendMail() {
+    var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    };
+    const serviceID = "service_port";
+    const templateID = "template_frj8mbc";
+    emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("your message sent successfully");
+    })
     }
-
-    emailjs.send("service_port","template_frj8mbc",parms).then(alert("Email Sent!!"))
-}
